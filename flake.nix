ac5926d37954dadb21c2869b0342f2e412ab5c2d
J Relly <dad@Js-MacBook-Pro.local>
2025-08-03 08:14:55 -0400

    container img fix - default.nix added to file

--- flake.nix
+++ flake.nix
@@ -6,15 +6,23 @@
   inputs.nix2container.url = "github:nlewo/nix2container";
 
   outputs = { self, nixpkgs, flake-utils, nix2container }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        python = pkgs.python311;
-        infra-watcher = import ./flake/pkgs/infra-watcher.nix { inherit pkgs python; };
-        container-img = import ./flake/container/default.nix { inherit pkgs infra-watcher nix2container; };
-      in {
-        packages.default = infra-watcher;
-        packages.container = container-img;
-        apps.default = flake-utils.lib.mkApp { drv = container-img; };
-      });
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs  = import nixpkgs { inherit system; };
+
+      python         = pkgs.python311;
+      infra-watcher  = import ./flake/pkgs/infra-watcher.nix { inherit pkgs python; };
+
+      # <-  HERE: pick the right attr set that owns `buildImage`
+      n2cPkgs = nix2container.packages.${system};
+
+      container-img = import ./flake/container/default.nix {
+        inherit pkgs infra-watcher;
+        nix2container = n2cPkgs;
+      };
+    in {
+      packages.default   = infra-watcher;
+      packages.container = container-img;
+      apps.default       = flake-utils.lib.mkApp { drv = container-img; };
+    });
 }
--- flake/container/default.nix
+++ flake/container/default.nix
@@ -1,15 +1,16 @@
+# flake/container/default.nix
 { pkgs, infra-watcher, nix2container }:
+
+# nix2container is the _package set_ that exposes `buildImage`
 nix2container.buildImage {
-  name = "registry.example.com/infra-watcher";
+  name = "infra-watcher";
   tag  = "0.1";
-  fromImage = pkgs.dockerTools.baseImage;
-  copyToRoot = [ infra-watcher ];
+
+  fromImage   = pkgs.dockerTools.baseImage;        # scratch-like layer
+  copyToRoot  = [ infra-watcher ./../assets ];     # agent + config
   config = {
-    Cmd = [ "/bin/sh" "-c" "infra-tailer & fluent-bit -c /assets/fluent-bit.conf" ];
-    ExposedPorts = { "8080/tcp" = {}; }; # health port
-    Healthcheck = {
-      Test = [ "CMD" "curl" "-f" "http://localhost:8080/status" ];
-      Interval = "30s"; Timeout = "3s"; Retries = 3;
-    };
+    Cmd          = [ "/bin/sh" "-c"
+                     "infra-tailer & fluent-bit -c /assets/fluent-bit.conf" ];
+    ExposedPorts = { "8080/tcp" = {}; };
   };
 }
